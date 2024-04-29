22c33897ca55a3e88211d7496c9ff50f06ec8d51
Volker <admin@bernd-mausehund.de>
2024-04-29 19:49:25 +0200

    format

--- logger.js
+++ logger.js
@@ -12,6 +12,13 @@ export const logger = pino({
   base: undefined,
   level: process.env.PINO_LOG_LEVEL || 'trace',
   timestamp: pino.stdTimeFunctions.isoTime,
+  formatters: {
+    log (obj) {
+      obj.foo='bar';
+      delete  obj.log;
+      return (obj)
+    }
+  },
   transport: {
     target:'pino-pretty',
       options: {
