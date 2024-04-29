7459ddd2770a6362fe5100f7ae2e7a324b623ad4
fontanero42 <admin@bernd-mausehund.de>
2024-04-29 21:03:24 +0200

    merged

--- experiment.js
+++ experiment.js
@@ -19,9 +19,6 @@ export function createExperiment() {
     experiment.out = function () {
         this.log.debug("go int");
     }
-get    essential(){
-        return{val:this.valor,col: this.color};
-    }
     return experiment;
 }
 
@@ -36,3 +33,4 @@ ex2.init('red', 0);
 ex2.out();
 ex.valor = 10;
 console.log(ex.valor);
+logger.debug(ex,'try');
