54a16bc7b225f5305f97e60d2e1e128c659124c6
fontanero42 <admin@bernd-mausehund.de>
2024-05-08 19:27:35 +0200

    exp

--- experiment.js
+++ experiment.js
@@ -1,5 +1,5 @@
 import { logger } from "./logger.js";
-console.log(logger);
+//console.log(logger);
 /*logger.formatters ={
     log (obj) {
       obj.foo='bar';
@@ -10,22 +10,24 @@ console.log(logger);
 
 export function createExperiment() {
     let experiment = Object.create(null);
-    experiment.color = "green";
-    experiment.init = function (color, valor) {
+    let color = "green";
+    let valor = 0;
+    const init = function (color, valor) {
         this.color = color;
         this.valor = valor;
         this.log = logger.child({ test: 'test', color: this.color, valor: this.valor });
     }
-    experiment.out = function () {
+      const out = function () {
         this.log.debug("go int");
     }
-    return experiment;
+    const proxy= {farbe: color, wert: valor};
+    return {experiment, valor, color, init,out,proxy}
 }
 
 let ex = createExperiment();
 console.log(ex.color);
 ex.init('blue', 8);
-console.log(ex.color);
+console.log(ex.color);                                                                            
 ex.out();
 console.log(ex.valor);
 let ex2 = createExperiment();
@@ -34,3 +36,4 @@ ex2.out();
 ex.valor = 10;
 console.log(ex.valor);
 logger.debug(ex,'try');
+logger.debug(ex.proxy,'stellvertreter');
